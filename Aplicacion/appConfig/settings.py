d0f4d42873f9ac6cf23eb65f4ba020f1386d62f1
“GaRIEN” <gabrielhuaman683@gmail.com>
2024-04-06 22:40:43 -0500

    configure la database

--- Aplicacion/appConfig/settings.py
+++ Aplicacion/appConfig/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'appConfig.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'project_venta',  
+        'USER': 'remote',   
+        'PASSWORD': '1234', 
+        'HOST': '192.168.18.5',  
+        'PORT': '3306',   
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
